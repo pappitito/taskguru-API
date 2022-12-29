7750aa33e417a7af0617d03e16d1bc2dffd0b321
Tito <107757681+pappitito@users.noreply.github.com>
2022-12-29 21:10:05 +0100

    Update authController.js

--- server/authController.js
+++ server/authController.js
@@ -9,35 +9,50 @@ const register = async (req,res) => {
     const {name, email, password} = req.body
 
     if(!name || !email || !password){
-        return res.status(StatusCodes.BAD_REQUEST).send('please input name email and password')
-    }
-  /*  const salt = await bcrypt.genSalt(10)
-    const HashedPassword = await bcrypt.hash(password, salt)
+        return res.status(StatusCodes.BAD_REQUEST).json({err: 'please input complete feild input'})
 
-    const tempUser = {name, email, password: HashedPassword} 
-    */
-    if(password.length < 6){
-        return res.status(StatusCodes.BAD_REQUEST).send('password must be greater than 6')
     }
-    const user = await User.create(req.body)
-    const token = user.createToken()
-    res.status(StatusCodes.CREATED).json({user: {name: user.name}, token})
+    if(password.length < 5){
+        return res.status(StatusCodes.BAD_REQUEST).json({err: 'password should be longer than five characters'})
+    }
 
+    try {
+        const user = await User.create(req.body)
+        const token = await user.createToken()
+        return res.status(StatusCodes.CREATED).json({user: {name: user.name}, token})
+    } catch (error) {
+        
+        if(error.code === 11000){
+            return res.status(StatusCodes.BAD_REQUEST).json({err: 'email already in use'})
+        }
+        if(error._message === "user validation failed"){
+            return res.status(StatusCodes.BAD_REQUEST).json({err: 'please input valid email'})
+        }
+        
+        return res.status(StatusCodes.BAD_REQUEST).json({err: error})
+        
+    }
 }
 
 const login = async (req,res) => {
     const {email, password} = req.body
     if(!email || !password){
-        return res.status(StatusCodes.BAD_REQUEST).send('please input email and password')
+        return res.status(StatusCodes.BAD_REQUEST).send({err: "email and password required"})
     }
     const user = await User.findOne({email: email})
-    const passwordCorrect = await user.checkPassword(password)
+    var passwordCorrect
+    try {
+         passwordCorrect = await user.checkPassword(password)
+    } catch (error) {
+        
+        return res.status(StatusCodes.UNAUTHORIZED).send({err: "Please provide a valid email"})
+    }
     if(!user){
-        return res.status(StatusCodes.UNAUTHORIZED).send('Invalid useranme or password')
+        return res.status(StatusCodes.UNAUTHORIZED).send({err: "Invalid username or password"})
     }
     
     if(!passwordCorrect){
-        return res.status(StatusCodes.UNAUTHORIZED).send('Invalid useranme or password')
+        return res.status(StatusCodes.UNAUTHORIZED).send({err: "Invalid username or password"})
     }
     const token = user.createToken()
     res.status(StatusCodes.OK).json({user: {name: user.name}, token})
